50b127622bad3ebddde5170ac29586ccbdacdbd6
Karan Bhatia <karan.01819011722@ipu.ac.in>
2024-10-27 21:36:15 +0530

    changes

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -11,7 +12,7 @@ class Expenses extends StatefulWidget {
   }
 }
 
-class _ExpensesState extends State {
+class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
     Expense(
       title: 'Flutter Course',
@@ -27,14 +28,25 @@ class _ExpensesState extends State {
     ),
   ];
 
+  void _openAddExpenseOverlay(){
+    showModalBottomSheet(context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [],
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: Column(
         children: [
+          // Toolbar with the Add Button => Row()
           const Text("The Chart"),
           Expanded(
             child: ExpensesList(expenses: _registeredExpenses),
